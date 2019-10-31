6f8e9550fc5039158eb0fd1ea9e2c903ac83b8e5
5a1em <luckybattle@yandex.ru>
2019-10-31 13:37:45 +0400

    added crud operations for tours table

---
+++ AdminPanel/Controllers/ToursController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdminPanel.Models;
+
+namespace AdminPanel.Controllers
+{
+    public class ToursController : Controller
+    {
+        private DatabaseEntities db = new DatabaseEntities();
+
+        // GET: Tours
+        public ActionResult Index()
+        {
+            var tours = db.Tours.Include(t => t.City).Include(t => t.City1);
+            return View(tours.ToList());
+        }
+
+        // GET: Tours/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // GET: Tours/Create
+        public ActionResult Create()
+        {
+            ViewBag.FromCityId = new SelectList(db.Cities, "CityId", "Name");
+            ViewBag.ToCityId = new SelectList(db.Cities, "CityId", "Name");
+            return View();
+        }
+
+        // POST: Tours/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TourId,Price,StartDate,EndDate,FromCityId,ToCityId,Image")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tours.Add(tour);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FromCityId = new SelectList(db.Cities, "CityId", "Name", tour.FromCityId);
+            ViewBag.ToCityId = new SelectList(db.Cities, "CityId", "Name", tour.ToCityId);
+            return View(tour);
+        }
+
+        // GET: Tours/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FromCityId = new SelectList(db.Cities, "CityId", "Name", tour.FromCityId);
+            ViewBag.ToCityId = new SelectList(db.Cities, "CityId", "Name", tour.ToCityId);
+            return View(tour);
+        }
+
+        // POST: Tours/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TourId,Price,StartDate,EndDate,FromCityId,ToCityId,Image")] Tour tour)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tour).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FromCityId = new SelectList(db.Cities, "CityId", "Name", tour.FromCityId);
+            ViewBag.ToCityId = new SelectList(db.Cities, "CityId", "Name", tour.ToCityId);
+            return View(tour);
+        }
+
+        // GET: Tours/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tour tour = db.Tours.Find(id);
+            if (tour == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tour);
+        }
+
+        // POST: Tours/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tour tour = db.Tours.Find(id);
+            db.Tours.Remove(tour);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
