11bc80fbec1c5958581d74cdc901e2b55dc88f31
5a1em <luckybattle@yandex.ru>
2019-10-31 13:21:15 +0400

    added crud operations for regions table

---
+++ AdminPanel/Controllers/RegionsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdminPanel.Models;
+
+namespace AdminPanel.Controllers
+{
+    public class RegionsController : Controller
+    {
+        private DatabaseEntities db = new DatabaseEntities();
+
+        // GET: Regions
+        public ActionResult Index()
+        {
+            var regions = db.Regions.Include(r => r.Country);
+            return View(regions.ToList());
+        }
+
+        // GET: Regions/Create
+        public ActionResult Create()
+        {
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Name");
+            return View();
+        }
+
+        // POST: Regions/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RegionId,Name,CountryId")] Region region)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Regions.Add(region);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Name", region.CountryId);
+            return View(region);
+        }
+
+        // GET: Regions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Region region = db.Regions.Find(id);
+            if (region == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Name", region.CountryId);
+            return View(region);
+        }
+
+        // POST: Regions/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RegionId,Name,CountryId")] Region region)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(region).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CountryId = new SelectList(db.Countries, "CountryId", "Name", region.CountryId);
+            return View(region);
+        }
+
+        // GET: Regions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Region region = db.Regions.Find(id);
+            if (region == null)
+            {
+                return HttpNotFound();
+            }
+            return View(region);
+        }
+
+        // POST: Regions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Region region = db.Regions.Find(id);
+            db.Regions.Remove(region);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
