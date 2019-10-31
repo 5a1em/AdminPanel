b891240dae081d4cb48b4ab0774d4330fdf0c471
5a1em <luckybattle@yandex.ru>
2019-10-31 13:28:02 +0400

    added crud operations for cities table

---
+++ AdminPanel/Controllers/CitiesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AdminPanel.Models;
+
+namespace AdminPanel.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private DatabaseEntities db = new DatabaseEntities();
+
+        // GET: Cities
+        public ActionResult Index()
+        {
+            var cities = db.Cities.Include(c => c.Region);
+            return View(cities.ToList());
+        }
+
+        // GET: Cities/Create
+        public ActionResult Create()
+        {
+            ViewBag.RegionId = new SelectList(db.Regions, "RegionId", "Name");
+            return View();
+        }
+
+        // POST: Cities/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CityId,Name,RegionId")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cities.Add(city);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RegionId = new SelectList(db.Regions, "RegionId", "Name", city.RegionId);
+            return View(city);
+        }
+
+        // GET: Cities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RegionId = new SelectList(db.Regions, "RegionId", "Name", city.RegionId);
+            return View(city);
+        }
+
+        // POST: Cities/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CityId,Name,RegionId")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(city).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RegionId = new SelectList(db.Regions, "RegionId", "Name", city.RegionId);
+            return View(city);
+        }
+
+        // GET: Cities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            City city = db.Cities.Find(id);
+            if (city == null)
+            {
+                return HttpNotFound();
+            }
+            return View(city);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            City city = db.Cities.Find(id);
+            db.Cities.Remove(city);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
