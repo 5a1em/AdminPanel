a1b42b943c07e9f9f224cb11d34e84449c3768b1
5a1em <luckybattle@yandex.ru>
2019-11-07 16:33:52 +0400

    fixed css markup in page for viewing tours

--- AdminPanel/Controllers/HomeController.cs
+++ AdminPanel/Controllers/HomeController.cs
@@ -1,31 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Net;
 using System.Web.Mvc;
+using AdminPanel.Models;
 
 namespace AdminPanel.Controllers
 {
     public class HomeController : Controller
     {
+        private DatabaseEntities db = new DatabaseEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var tours = db.Tours.Include(t => t.City).Include(t => t.City1);
+            return View(tours.ToList());
         }
 
         [HttpPost]
         public ActionResult List(string fromCity, string toWhere, DateTime? startDate = null, int days = 0)
         {
-
-
-
-
-
-
-
-
-
-
             return View();
         }
     }
